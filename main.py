acf25ef6cab0e9a645591afec5cad0cc323ac7b7
Dominik Radic <dominik.radic@gmail.com>
2024-07-08 20:24:44 +1000

    refactored some code

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
-import os
 
 class SessionActionsExtension(Extension):
-
     def __init__(self):
         super(SessionActionsExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
-
 class KeywordQueryEventListener(EventListener):
-
     def on_event(self, event, extension):
         items = []
         options = [
